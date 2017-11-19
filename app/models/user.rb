1ab4dfc408e105108f6b34e3bafa9217fe26e489
chentianwei <ctw.3@163.com>
2017-11-19 18:12:57 +0800

    implement admin/jobs curd and so on

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  def admin?
+    is_admin
+    # 在user中 add_column :users, :is_admin, :boolean, default: false
+  end
 end
