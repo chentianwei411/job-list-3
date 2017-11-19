955ef3cbb335fe9a2c4c2996af43af7b716fdc06
chentianwei <ctw.3@163.com>
2017-11-19 17:10:53 +0800

    add job curd

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171118033504) do
+ActiveRecord::Schema.define(version: 20171119061628) do
+
+  create_table "jobs", force: :cascade do |t|
+    t.string   "title"
+    t.text     "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
