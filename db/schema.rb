7914ed31482fc44032e64f941a8f9478a3952c39
chentianwei <ctw.3@163.com>
2017-12-01 11:27:38 +0800

    upload resume

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171201015020) do
+ActiveRecord::Schema.define(version: 20171201024742) do
 
   create_table "jobs", force: :cascade do |t|
     t.string   "title"
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 20171201015020) do
     t.string   "attachment"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "job_id"
   end
 
   create_table "users", force: :cascade do |t|
